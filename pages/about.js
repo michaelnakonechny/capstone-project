496ff9b875c8ef2f030df9351403014bb42cf861
Michael Nakonechny <michaelnako@icloud.com>
2022-10-04 14:56:00 +0200

    test

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,3 @@
 export default function About() {
-  return (
-    <>
-      <h1>This page was made with a lot of pain</h1>
-    </>
-  );
+  return <h1>This page was made with a lot of pain</h1>;
 }
