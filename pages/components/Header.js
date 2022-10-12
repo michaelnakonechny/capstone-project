c4693c7b0437ae8958bc64d26a8b020df11900b5
Michael Nakonechny <michaelnako@icloud.com>
2022-10-12 23:16:35 +0200

    pblogo

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,6 +1,8 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import Head from 'next/head';
 import MainMenu from './Mainmenu';
+import Pblogo from '../../public/pblogo.png';
+import Image from 'next/image';
 
 export default function Header() {
   return (
@@ -11,6 +13,10 @@ export default function Header() {
       <AppBar position="fixed">
         <Toolbar>
           <MainMenu />
+          <div>
+            <Image width="55px" height="55px" src={Pblogo} alt={Pblogo} />
+          </div>
+
           <Typography variant="h6" color="secondary">
             PARKBENCH BAGELS
           </Typography>
